71d78c988e2999ee3b6a8b7126511c0c5efb8028
babiga <babiga88@gmail.com>
2026-02-02 21:21:21 +0800

    up

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,9 +2,19 @@ import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@/generated/prisma/client";
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
+const connectionString = process.env.DATABASE_URL!;
 
-export { prisma };
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter: new PrismaPg({ connectionString }),
+    log: ["error"],
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
